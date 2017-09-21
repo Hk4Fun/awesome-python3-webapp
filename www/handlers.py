e127d6cd7a02fb4fd1bc158d4ec4cb122d3ab1aa
99nobody <941222165chenhongwen@gmail.com>
2017-09-21 22:00:51 +0800

    使用uikit作为前端框架，编写api，加入父模板

--- www/handlers.py
+++ www/handlers.py
@@ -13,13 +13,32 @@ from webframe import get, post
 
 from models import User, Comment, Blog, next_id
 
+from apis import APIResourceNotFoundError, APIValueError, APIError, APIPermissionError, Page
+
+
+# 取得页码
+def get_page_index(page_str):
+    # 将传入的字符串转为页码信息, 实际只是对传入的字符串做了合法性检查（容错处理）
+    p = 1
+    try:
+        p = int(page_str)
+    except ValueError as e:
+        pass
+    if p < 1:
+        p = 1
+    return p
 
 @get('/')
-async def index(request):
-    users = await User.findAll()
+def index(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
     return {
-        '__template__': 'test.html',
-        'users': users
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
 
 @get('/greeting')
@@ -27,3 +46,16 @@ async def handler_url_greeting(*,name,request):
     body='<h1>Awesome: /greeting %s</h1>'%name
     return body
 
+# API: 获取用户信息
+@get('/api/users')
+async def api_get_users(*, page="1"):
+    page_index = get_page_index(page)
+    num = await User.findNumber("count(id)")#获取用户数量
+    p = Page(num, page_index)
+    if num == 0:
+        return dict(page=p, users=())
+    users = await User.findAll(orderBy="created_at desc")
+    for u in users:
+        u.passwd = "*****" # 将密码覆盖掉
+    # 以dict形式返回,并且未指定__template__,将被app.py的response factory处理为json
+    return dict(page=p, users=users)
